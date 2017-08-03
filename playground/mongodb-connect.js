8856ebd360a417e74f0156689c8da3d7b609a35f
vignesh4github <csevickee@gmail.com>
2017-08-02 18:48:15 -0700

    first commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const {MongoClient,ObjectID} = require('mongodb');
+
+// const MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://localhost:27017/Contact-manager', (err, db) =>
+{
+if(err)
+{
+return console.log('Connection coudnt get connected properly');
+}
+
+ console.log("Connected with the server successfully..!!!");
+
+// creating collections
+
+/* db.collection('contacts').insertOne(
+  {
+    id  : 123,
+    name : 'vickee',
+    hometown : 'salem'
+  }
+, (err, result) =>
+{
+  if(err)
+  {
+    console.log('unable to Insert into the collection',err);
+  }
+  console.log(result.ops[0].name);
+});*/
+
+//shutdown the db
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,26 @@
+const {MongoClient,ObjectID} = require('mongodb');
+
+// const MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://localhost:27017/Contact-manager', (err, db) =>
+{
+if(err)
+{
+return console.log('Connection coudnt get connected properly');
+}
+
+ console.log("Connected with the server successfully..!!!");
+
+db.collection('contacts').find().count().then((count) =>
+{
+
+console.log(`Contacts count : ${count}`);
+},(err) =>
+{
+  console.log('Unable to fetch the contacts',err);
+});
+
+//shutdown the db
+  db.close();
+});
